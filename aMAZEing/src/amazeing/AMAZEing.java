0c73ee0b0472ef2be8a25b1a3939db95b875840f
Victor Elsenaar <elsenaar@gmail.com>
2016-05-15 21:33:34 +0200

    Test commit through netbeans

--- aMAZEing/src/amazeing/AMAZEing.java
+++ aMAZEing/src/amazeing/AMAZEing.java
@@ -16,6 +16,7 @@ public class AMAZEing {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hello world");
     }
     
 }
