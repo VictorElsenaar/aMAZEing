d34428cc1b6172e91ea5632161a2e78ab52ed376
KahooWu <kahoo.wu@gmail.com>
2016-06-16 12:50:04 +0200

    Auteur

--- aMAZEing/test/amazeing/OptimaleRouteTest.java
+++ aMAZEing/test/amazeing/OptimaleRouteTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package amazeing;
 
 import static amazeing.AMAZEing.debug;
@@ -18,7 +13,7 @@ import static org.junit.Assert.*;
 
 /**
  *
- * @author vic
+ * @author Victor Elsenaar en Kahoo Wu
  */
 public class OptimaleRouteTest {
     
